fe334005b07620cce2e45e7b3e59c72298a06fd6
Jonathan Hong <jonhong777@gmail.com>
2018-07-04 03:12:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def long_planeteer_calls(calls)
   false
 end
 
-def find_the_cheese()
-  # the array below is here to help
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end

4c2a69db653c64ce62ab5609b90ee24ba8abfb77
Jonathan Hong <jonhong777@gmail.com>
2018-07-04 03:13:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,9 @@ end
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  cheese_types.each do |cheese|
+    if strings.include?(cheese)
+      return true
+    else
+  end
 end
